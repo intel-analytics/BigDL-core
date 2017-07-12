1fe67ffaaf8c0c7ddaeec7bcdc401ec788f2ca8a
Ian Wong <yiheng.wang@intel.com>
2017-07-12 10:42:59 -0400

    Support Win64 (#13)
    * support win64
    
    * add some comments
    
    * fix some error
    
    * add comment

--- mkl2017-xeon-blas/jni/src/main/java/com/intel/analytics/bigdl/mkl/MKL.java
+++ mkl2017-xeon-blas/jni/src/main/java/com/intel/analytics/bigdl/mkl/MKL.java
@@ -27,13 +27,24 @@ public class MKL {
             if (System.getProperty("os.name").toLowerCase().contains("mac")) {
                 iomp5FileName = "libiomp5.dylib";
                 jmklFileName = "libjmkl.dylib";
+            } else if(System.getProperty("os.name").toLowerCase().contains("win")) {
+                iomp5FileName = "libiomp5md.dll";
+                jmklFileName = "libjmkl.dll";
             }
+
             tmpFile = extract(iomp5FileName);
-            System.load(tmpFile.getAbsolutePath());
-            tmpFile.delete(); // delete so temp file after loaded
+            try {
+                System.load(tmpFile.getAbsolutePath());
+            } finally {
+                tmpFile.delete(); // delete so temp file after loaded
+            }
+
             tmpFile = extract(jmklFileName);
-            System.load(tmpFile.getAbsolutePath());
-            tmpFile.delete(); // delete so temp file after loaded
+            try {
+                System.load(tmpFile.getAbsolutePath());
+            } finally {
+                tmpFile.delete(); // delete so temp file after loaded
+            }
 
             setMklEnv();
 
@@ -269,18 +280,29 @@ public class MKL {
         try {
             URL url = MKL.class.getResource("/" + path);
             if (url == null) {
-                throw new Error("Can't find so file in jar, path = " + path);
+                throw new Error("Can't find dynamic lib file in jar, path = " + path);
             }
 
             InputStream in = MKL.class.getResourceAsStream("/" + path);
-            File file = createTempFile("dlNativeLoader", path);
+            File file = null;
+
+            // Windows won't allow to change the dll name, so we keep the name
+            // It's fine as windows is consider in a desktop env, so there won't multiple instance
+            // produce the dynamic lib file
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                file = new File(System.getProperty("java.io.tmpdir") + File.separator + path);
+            } else {
+                file = createTempFile("dlNativeLoader", path);
+            }
 
             ReadableByteChannel src = newChannel(in);
             FileChannel dest = new FileOutputStream(file).getChannel();
             dest.transferFrom(src, 0, Long.MAX_VALUE);
+            dest.close();
+            src.close();
             return file;
         } catch (Throwable e) {
-            throw new Error("Can't extract so file to /tmp dir");
+            throw new Error("Can't extract dynamic lib file to /tmp dir.\n" + e);
         }
     }
 
