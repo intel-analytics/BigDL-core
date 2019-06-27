c9f32719d86161b554a14a3bda2296bada5ff353
Wang Yanzhang <i8run15@gmail.com>
2019-06-27 11:08:58 +0800

    fix: affinity will not work on cgroup envs, such as in docker

--- native-dnn/src/test/java/com/intel/analytics/bigdl/mkl/AffinityTest.java
+++ native-dnn/src/test/java/com/intel/analytics/bigdl/mkl/AffinityTest.java
@@ -8,16 +8,21 @@ import static org.junit.Assert.assertTrue;
 public class AffinityTest {
     @Test
     public void SetSingleAffinity() {
+        int[] backup = Affinity.getAffinity();
+
         Affinity.setAffinity(3);
 
         int[] cores = Affinity.getAffinity();
 
         assertTrue(cores.length == 1);
         assertTrue(cores[0] == 3);
+
+        Affinity.setAffinity(backup);
     }
 
     @Test
     public void SetMultiAffinity() {
+        int[] backup = Affinity.getAffinity();
         int[] cores = {1, 2, 3};
         Affinity.setAffinity(cores);
 
@@ -27,16 +32,21 @@ public class AffinityTest {
         assertTrue(getCores[0] == 1);
         assertTrue(getCores[1] == 2);
         assertTrue(getCores[2] == 3);
+
+        Affinity.setAffinity(backup);
     }
 
     @Test
     public void SetOmpAffinity() {
+        int[] backup = Affinity.getAffinity();
         Affinity.setOmpAffinity();
 
         int[] cores = Affinity.getAffinity();
 
         assertTrue(cores.length == 1);
         assertTrue(cores[0] == 0);
+
+        Affinity.setAffinity(backup);
     }
 
 }
