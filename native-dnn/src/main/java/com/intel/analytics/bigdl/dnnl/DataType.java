e9fa852163d9e1fc157de1cdf0c25db3e4b3b4fe
Wang Yanzhang <i8run15@gmail.com>
2019-11-20 16:56:01 +0800

    fix: update datatype

--- native-dnn/src/main/java/com/intel/analytics/bigdl/dnnl/DataType.java
+++ native-dnn/src/main/java/com/intel/analytics/bigdl/dnnl/DataType.java
@@ -2,9 +2,10 @@ package com.intel.analytics.bigdl.dnnl;
 
 public class DataType {
     public static final int Undef = 0;
-    public static final int F32 = 1;
-    public static final int S32 = 2;
-    public static final int S16 = 4;
+    public static final int F16 = 1;
+    public static final int Bf16 = 2;
+    public static final int F32 = 3;
+    public static final int S32 = 4;
     public static final int S8 = 5;
     public static final int U8 = 6;
 }
