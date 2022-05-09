6714517c7e59cef9e53e1c69af798e00ccdeb44f
Qiyuan Gong <qiyuan.gong@intel.com>
2022-05-09 22:18:52 +0800

    Format code with clang format

--- ppml/src/main/cpp/quote_verification.cpp
+++ ppml/src/main/cpp/quote_verification.cpp
@@ -1,125 +1,119 @@
-#include <stdio.h>
-#include <vector>
-#include <string>
+#include "com_intel_analytics_bigdl_ppml_attestation_Attestation.h"
+#include "sgx_dcap_quoteverify.h"
+#include "sgx_ql_quote.h"
+#include "sgx_urts.h"
 #include <assert.h>
-#include <string.h>
 #include <fstream>
 #include <sgx_uae_launch.h>
-#include "sgx_urts.h"
-#include "sgx_ql_quote.h"
-#include "sgx_dcap_quoteverify.h"
-#include "com_intel_analytics_bigdl_ppml_attestation_Attestation.h"
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-
 /**
  * @param quote - ECDSA quote buffer
- * @param use_qve - Set quote verification mode
- *                   If true, quote verification will be performed by Intel QvE
- *                   If false, quote verification will be performed by untrusted QVL
  */
+int ecdsa_quote_verification(vector<uint8_t> quote) {
+  int ret = 0;
+  time_t current_time = 0;
+  uint32_t supplemental_data_size = 0;
+  uint8_t *p_supplemental_data = NULL;
+  // sgx_status_t sgx_ret = SGX_SUCCESS;
+  quote3_error_t dcap_ret = SGX_QL_ERROR_UNEXPECTED;
+  sgx_ql_qv_result_t quote_verification_result = SGX_QL_QV_RESULT_UNSPECIFIED;
+  // sgx_ql_qe_report_info_t qve_report_info;
+  uint32_t collateral_expiration_status = 1;
 
-int ecdsa_quote_verification(vector<uint8_t> quote)
-{
-    int ret = 0;
-    time_t current_time = 0;
-    uint32_t supplemental_data_size = 0;
-    uint8_t *p_supplemental_data = NULL;
-    // sgx_status_t sgx_ret = SGX_SUCCESS;
-    quote3_error_t dcap_ret = SGX_QL_ERROR_UNEXPECTED;
-    sgx_ql_qv_result_t quote_verification_result = SGX_QL_QV_RESULT_UNSPECIFIED;
-    // sgx_ql_qe_report_info_t qve_report_info;
-    // unsigned char rand_nonce[16] = "59jslk201fgjmm;";
-    uint32_t collateral_expiration_status = 1;
+  // int updated = 0;
+  // quote3_error_t verify_qveid_ret = SGX_QL_ERROR_UNEXPECTED;
+  // sgx_enclave_id_t eid = 0;
+  // sgx_launch_token_t token = { 0 };
 
-    // int updated = 0;
-    // quote3_error_t verify_qveid_ret = SGX_QL_ERROR_UNEXPECTED;
-    // sgx_enclave_id_t eid = 0;
-    // sgx_launch_token_t token = { 0 };
+  // Untrusted quote verification
 
-    // Untrusted quote verification
+  // call DCAP quote verify library to get supplemental data size
+  dcap_ret = sgx_qv_get_quote_supplemental_data_size(&supplemental_data_size);
+  if (dcap_ret == SGX_QL_SUCCESS &&
+      supplemental_data_size == sizeof(sgx_ql_qv_supplemental_t)) {
+    printf("\tInfo: sgx_qv_get_quote_supplemental_data_size successfully "
+           "returned.\n");
+    p_supplemental_data = (uint8_t *)malloc(supplemental_data_size);
+  } else {
+    if (dcap_ret != SGX_QL_SUCCESS)
+      printf(
+          "\tError: sgx_qv_get_quote_supplemental_data_size failed: 0x%04x\n",
+          dcap_ret);
 
-    //call DCAP quote verify library to get supplemental data size
-    //
-    dcap_ret = sgx_qv_get_quote_supplemental_data_size(&supplemental_data_size);
-    if (dcap_ret == SGX_QL_SUCCESS && supplemental_data_size == sizeof(sgx_ql_qv_supplemental_t)) {
-        printf("\tInfo: sgx_qv_get_quote_supplemental_data_size successfully returned.\n");
-        p_supplemental_data = (uint8_t*)malloc(supplemental_data_size);
-    }
-    else {
-        if (dcap_ret != SGX_QL_SUCCESS)
-            printf("\tError: sgx_qv_get_quote_supplemental_data_size failed: 0x%04x\n", dcap_ret);
+    if (supplemental_data_size != sizeof(sgx_ql_qv_supplemental_t))
+      printf("\tWarning: sgx_qv_get_quote_supplemental_data_size returned size "
+             "is not same with header definition in SGX SDK, please make sure "
+             "you are using same version of SGX SDK and DCAP QVL.\n");
 
-        if (supplemental_data_size != sizeof(sgx_ql_qv_supplemental_t))
-            printf("\tWarning: sgx_qv_get_quote_supplemental_data_size returned size is not same with header definition in SGX SDK, please make sure you are using same version of SGX SDK and DCAP QVL.\n");
+    supplemental_data_size = 0;
+  }
 
-        supplemental_data_size = 0;
-    }
+  // set current time. This is only for sample purposes, in production mode a
+  // trusted time should be used.
+  current_time = time(NULL);
 
-    //set current time. This is only for sample purposes, in production mode a trusted time should be used.
-    //
-    current_time = time(NULL);
+  // call DCAP quote verify library for quote verification
+  // here you can choose 'trusted' or 'untrusted' quote verification by
+  // specifying parameter '&qve_report_info' if '&qve_report_info' is NOT NULL,
+  // this API will call Intel QvE to verify quote if '&qve_report_info' is NULL,
+  // this API will call 'untrusted quote verify lib' to verify quote, this mode
+  // doesn't rely on SGX capable system, but the results can not be
+  // cryptographically authenticated
+  dcap_ret = sgx_qv_verify_quote(quote.data(), (uint32_t)quote.size(), NULL,
+                                 current_time, &collateral_expiration_status,
+                                 &quote_verification_result, NULL,
+                                 supplemental_data_size, p_supplemental_data);
+  if (dcap_ret == SGX_QL_SUCCESS) {
+    printf("\tInfo: App: sgx_qv_verify_quote successfully returned.\n");
+  } else {
+    printf("\tError: App: sgx_qv_verify_quote failed: 0x%04x\n", dcap_ret);
+  }
 
-
-    //call DCAP quote verify library for quote verification
-    //here you can choose 'trusted' or 'untrusted' quote verification by specifying parameter '&qve_report_info'
-    //if '&qve_report_info' is NOT NULL, this API will call Intel QvE to verify quote
-    //if '&qve_report_info' is NULL, this API will call 'untrusted quote verify lib' to verify quote, this mode doesn't rely on SGX capable system, but the results can not be cryptographically authenticated
-    dcap_ret = sgx_qv_verify_quote(
-        quote.data(), (uint32_t)quote.size(),
-        NULL,
-        current_time,
-        &collateral_expiration_status,
-        &quote_verification_result,
-        NULL,
-        supplemental_data_size,
-        p_supplemental_data);
-    if (dcap_ret == SGX_QL_SUCCESS) {
-        printf("\tInfo: App: sgx_qv_verify_quote successfully returned.\n");
-    }
-    else {
-        printf("\tError: App: sgx_qv_verify_quote failed: 0x%04x\n", dcap_ret);
-    }
-
-    //check verification result
-    //
-    switch (quote_verification_result)
-    {
-    case SGX_QL_QV_RESULT_OK:
-        printf("\tInfo: App: Verification completed successfully.\n");
-        ret = 0;
-        break;
-    case SGX_QL_QV_RESULT_CONFIG_NEEDED:
-    case SGX_QL_QV_RESULT_OUT_OF_DATE:
-    case SGX_QL_QV_RESULT_OUT_OF_DATE_CONFIG_NEEDED:
-    case SGX_QL_QV_RESULT_SW_HARDENING_NEEDED:
-    case SGX_QL_QV_RESULT_CONFIG_AND_SW_HARDENING_NEEDED:
-        printf("\tWarning: App: Verification completed with Non-terminal result: %x\n", quote_verification_result);
-        ret = 1;
-        break;
-    case SGX_QL_QV_RESULT_INVALID_SIGNATURE:
-    case SGX_QL_QV_RESULT_REVOKED:
-    case SGX_QL_QV_RESULT_UNSPECIFIED:
-    default:
-        printf("\tError: App: Verification completed with Terminal result: %x\n", quote_verification_result);
-        ret = -1;
-        break;
-    }
-
-    return ret;
+  // check verification result
+  switch (quote_verification_result) {
+  case SGX_QL_QV_RESULT_OK:
+    printf("\tInfo: App: Verification completed successfully.\n");
+    ret = 0;
+    break;
+  case SGX_QL_QV_RESULT_CONFIG_NEEDED:
+  case SGX_QL_QV_RESULT_OUT_OF_DATE:
+  case SGX_QL_QV_RESULT_OUT_OF_DATE_CONFIG_NEEDED:
+  case SGX_QL_QV_RESULT_SW_HARDENING_NEEDED:
+  case SGX_QL_QV_RESULT_CONFIG_AND_SW_HARDENING_NEEDED:
+    printf(
+        "\tWarning: App: Verification completed with Non-terminal result: %x\n",
+        quote_verification_result);
+    ret = 1;
+    break;
+  case SGX_QL_QV_RESULT_INVALID_SIGNATURE:
+  case SGX_QL_QV_RESULT_REVOKED:
+  case SGX_QL_QV_RESULT_UNSPECIFIED:
+  default:
+    printf("\tError: App: Verification completed with Terminal result: %x\n",
+           quote_verification_result);
+    ret = -1;
+    break;
+  }
+  return ret;
 }
 
-JNIEXPORT jint JNICALL Java_com_intel_analytics_bigdl_ppml_attestation_Attestation_sdkVerifyQuote
-  (JNIEnv * env, jclass cls, jbyteArray quote) {
-    //convert jbyteArray to vector<char>
-    jbyte* jbae = env->GetByteArrayElements(quote, 0);
-    jsize len = env->GetArrayLength(quote);
-    char * quote_arrary = (char *)jbae;
-    vector<unsigned char> quote_vector;
-    for (int i = 0; i < len; i++) {
-        quote_vector.push_back(quote_arrary[i]);
-    }
-    int result = ecdsa_quote_verification(quote_vector);
-    return result;
+JNIEXPORT jint JNICALL
+Java_com_intel_analytics_bigdl_ppml_attestation_Attestation_sdkVerifyQuote(
+    JNIEnv *env, jclass cls, jbyteArray quote) {
+  // convert jbyteArray to vector<char>
+  jbyte *jbae = env->GetByteArrayElements(quote, 0);
+  jsize len = env->GetArrayLength(quote);
+  char *quote_arrary = (char *)jbae;
+  vector<unsigned char> quote_vector;
+  for (int i = 0; i < len; i++) {
+    quote_vector.push_back(quote_arrary[i]);
+  }
+  int result = ecdsa_quote_verification(quote_vector);
+  return result;
 }
