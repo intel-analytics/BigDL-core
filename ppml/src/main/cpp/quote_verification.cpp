1dd496ed059350190a0034a38b61076f1dc6905e
Qiyuan Gong <qiyuan.gong@intel.com>
2022-05-11 08:51:09 +0800

    Add np handle

--- ppml/src/main/cpp/quote_verification.cpp
+++ ppml/src/main/cpp/quote_verification.cpp
@@ -106,6 +106,8 @@ int ecdsa_quote_verification(vector<uint8_t> quote) {
 JNIEXPORT jint JNICALL
 Java_com_intel_analytics_bigdl_ppml_attestation_Attestation_sdkVerifyQuote(
     JNIEnv *env, jclass cls, jbyteArray quote) {
+  // Return -1 if quote is null
+  if (quote == NULL) return -1;
   // convert jbyteArray to vector<char>
   jbyte *jbae = env->GetByteArrayElements(quote, 0);
   jsize len = env->GetArrayLength(quote);
