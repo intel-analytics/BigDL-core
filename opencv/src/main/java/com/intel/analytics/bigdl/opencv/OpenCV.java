8edcdc8d61e540f2824d275809b17d89eafd1621
Le-Zheng <alettazheng@hotmail.com>
2022-07-25 18:05:19 +0800

    add opencv core

--- opencv/src/main/java/com/intel/analytics/bigdl/opencv/OpenCV.java
+++ opencv/src/main/java/com/intel/analytics/bigdl/opencv/OpenCV.java
@@ -49,7 +49,11 @@ public class OpenCV {
                 if (System.getProperty("os.name").toLowerCase().contains("mac")) {
                     LIBS = new String[]{
                             "libopencv_java420.dylib",
-                            "libpng16.16.dylib"};
+                            "libopencv_core.4.2.0.dylib",
+                            "libopencv_imgcodecs.4.2.0.dylib", 
+                            "libopencv_imgproc.4.2.0.dylib"
+                            "libopencv_java420.dylib",
+                    };
                 }
                 // TODO for windows, we don't create mkl.native dir
                 Path tempDir = null;
