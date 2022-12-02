c7dde956ca91381add6b3b010b3c81e0d31e898d
Xiangyu Tian <oveoveove142@outlook.com>
2022-12-02 13:52:39 +0800

    Fix

--- ppml-tdx/ppml-java-x86_64-linux/download_ppml.sh
+++ ppml-tdx/ppml-java-x86_64-linux/download_ppml.sh
@@ -5,12 +5,12 @@ set -ex
 if [ $PPML_BUILD = true ]; then 
     make 
 else
-    rm -f ./ppml-target.tar.gz
-    wget http://10.239.45.10:8081/repository/raw/bigdl-core/ppml-target.tar.gz
-    mkdir ppml-target
-    tar -zxvf ./ppml-target.tar.gz -C ppml-target
+    rm -f ./ppml-tdx-target.tar.gz
+    wget http://10.239.45.10:8081/repository/raw/bigdl-core/ppml-tdx-target.tar.gz
+    mkdir ppml-tdx-target
+    tar -zxvf ./ppml-tdx-target.tar.gz -C ppml-tdx-target
     rm -rf ./target
-    mv ./ppml-target/target/ ./
-    rm -rf ppml-target
-    rm -f ppml-target.tar.gz
+    mv ./ppml-tdx-target/target/ ./
+    rm -rf ppml-tdx-target
+    rm -f ppml-tdx-target.tar.gz
 fi
